682e457446c36ca1361a9df811e0d3b39e071f7d
Edwin Abreu <edwin_abreu86@hotmail.com>
2018-01-21 15:54:45 -0400

    Replaced ListView with RecyclerView. Added AboutAuthor Activity. Added OTBooks and NTBooks.

---
+++ app/src/main/java/com/edwin/abreusoft/manualbiblicoparacreyentes/AuthorActivity.java
@@ -0,0 +1,29 @@
+package com.edwin.abreusoft.manualbiblicoparacreyentes;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class AuthorActivity extends AppCompatActivity {
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_author);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        TextView authorText = findViewById(R.id.author_text);
+        authorText.setText(Paragraphs.ABOUT_AUTHOR);
+    }
+}
