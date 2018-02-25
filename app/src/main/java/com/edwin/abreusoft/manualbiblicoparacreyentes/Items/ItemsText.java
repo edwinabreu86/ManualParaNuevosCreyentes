94440a7bc9aa3336ed069031e785ac1573f340b5
Edwin Abreu <edwin_abreu86@hotmail.com>
2018-02-24 20:14:29 -0400

    Replaced ListView with RecyclerView. Added AboutAuthor Activity. Added OTBooks and NTBooks.

--- app/src/main/java/com/edwin/abreusoft/manualbiblicoparacreyentes/Items/ItemsText.java
+++ app/src/main/java/com/edwin/abreusoft/manualbiblicoparacreyentes/Items/ItemsText.java
@@ -8,23 +8,23 @@ public class ItemsText {
             {"¿En cuántas partes se divide la Biblia?", "En dos partes"},
             {"¿Cuáles son esas partes?", "Antiguo testamento y nuevo testamento"},
             {"¿Cuántas son las subdivisiones de la Biblia?", "10 subdivisiones"},
-            {"¿Cuáles son?", "1. Los libros del pentateuco \n" +
-                    "2. Los libros históricos del antiguo testamento \n" +
-                    "3. Los libros poéticos del antiguo testamento \n" +
-                    "4. Los profetas mayores del antiguo testamento \n" +
-                    "5. Los profetas menores del antiguo testamento \n" +
+            {"¿Cuáles son?", "1. Libros del pentateuco \n" +
+                    "2. Libros históricos del antiguo testamento \n" +
+                    "3. Libros poéticos del antiguo testamento \n" +
+                    "4. Libros proféticos mayores del antiguo testamento \n" +
+                    "5. Libros proféticos menores del antiguo testamento \n" +
                     "6. Los evangelios, que pueden ser históricos del nuevo testamento \n" +
-                    "7. El libro histórico del nuevo testamento \n" +
-                    "8. Las cartas paulinas del nuevo testamento \n" +
-                    "9. Las cartas generales del nuevo testamento \n" +
-                    "10. El libro profético del nuevo testamento"},
+                    "7. Libro histórico del nuevo testamento \n" +
+                    "8. Cartas paulinas del nuevo testamento \n" +
+                    "9. Cartas generales del nuevo testamento \n" +
+                    "10. Libro profético del nuevo testamento"},
             {"¿Cómo se le llama a los primeros cinco libros de la Biblia?", "Los libros del pentateuco"},
-            {"¿Cuáles son los nombres de los primeros cinco libros de la Biblia?", "Génesis, Éxodo, Levítico, Número y Deuteronomio"},
+            {"¿Cuáles son los libros del pentateuco?", "Génesis, Éxodo, Levítico, Número y Deuteronomio"},
             {"¿Cuántos son los libros históricos del antiguo testamento?", "12 libros"},
-            {"¿Cuáles son?", "1. Josué \n2. Jueces \n3. Ruth \n4. Samuel 1 \n5. Samuel 2 \n6. Reyes 1 \n" +
-                    "7.	Reyes 2 \n8. Crónicas 1 \n9. Crónicas 2 \n10. Esdras \n11. Nehemías \n12. Ester"},
+            {"¿Cuáles son?", "1. Josué \n2. Jueces \n3. Ruth \n4. 1ro de Samuel \n5. 2do de Samuel \n6. 1ro de Reyes \n" +
+                    "7.	2do de Reyes \n8. 1ro de Crónicas \n9. 2do de Crónicas \n10. Esdras \n11. Nehemías \n12. Ester"},
             {"¿Cuántos son los libros poéticos del antiguo testamento?", "5 libros"},
-            {"¿Cuáles son?", "1. Job \n2. Salmos \n3. Proverbios \n4. Eclesiastes \n5. cantar de los cantares"},
+            {"¿Cuáles son?", "1. Job \n2. Salmos \n3. Proverbios \n4. Eclesiastés \n5. Cantar de los cantares"},
             {"¿Cuántos son los libros de profetas mayores del antiguo testamento?", "5 libros de profetas mayores"},
             {"¿Cuáles son?", "1. Isaías \n2. Jeremías \n3. Lamentaciones \n4. Ezequiel \n5. Daniel"},
             {"¿Cuántos son los profetas menores del antiguo testamento?", "12 libros de profetas menores"},
@@ -34,10 +34,10 @@ public class ItemsText {
             {"¿Cuáles son?", "1. Mateo \n2. Marcos \n3. Lucas \n4. Juan"},
             {"¿Cuál otro libro histórico tiene el nuevo testamento, aparte de los evangelios?", "El libro de hechos de los apóstoles"},
             {"¿Cuántas son las cartas paulinas del nuevo testamento?", "13 cartas paulinas"},
-            {"¿Cuáles son?", "1. Romanos \n2. 1 Corintios \n3. 2 Corintios \n4. Gálatas \n5. Efesios \n6. Filipenses \n7. Colosenses \n" +
-                    "8. 1 Tesalonicenses \n9. 2 Tesalonicenses \n10. 1 Timoteo \n11. 2 Timoteo \n12. Tito \n13.	Filemón"},
+            {"¿Cuáles son?", "1. Romanos \n2. 1ra de Corintios \n3. 2da de Corintios \n4. Gálatas \n5. Efesios \n6. Filipenses \n7. Colosenses \n" +
+                    "8. 1ra de Tesalonicenses \n9. 2da de Tesalonicenses \n10. 1ra de Timoteo \n11. 2da de Timoteo \n12. Tito \n13.	Filemón"},
             {"¿Cuántas son las cartas generales del nuevo testamento incluyendo hebreo?", "8 Cartas generales incluyendo a hebreos"},
-            {"¿Cuáles son?", "1. Hebreos \n2. Santiago \n3. 1 Pedro \n4. 2 Pedro \n5. 1 Juan \n6. 2 Juan \n7. 3 Juan \n8. Judas"},
+            {"¿Cuáles son?", "1. Hebreos \n2. Santiago \n3. 1ra de Pedro \n4. 2da de Pedro \n5. 1ra de Juan \n6. 2da de Juan \n7. 3ra de Juan \n8. Judas"},
             {"¿Cuál es el libro profético del nuevo testamento?", "Apocalipsis"}};
 
     public static final String[][] TEXTS_TO_MEM = {
